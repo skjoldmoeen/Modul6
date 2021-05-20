ca6dee7f999a8d0cc397eba8f9af0f186bc74faf
loevgreen <s.loevgreen@gmail.com>
2021-05-20 15:03:31 +0200

    Indhold

--- pernille.js
+++ pernille.js
@@ -13,7 +13,7 @@ function myFunction() {
 
 window.onclick = function(e) {
   if (!e.target.matches('.dropbtn')) {
-  var myDropdown = document.getElementById("myDropdown");
+  let  myDropdown = document.getElementById("myDropdown");
     if (myDropdown.classList.contains('show')) {
       myDropdown.classList.remove('show');
     }
