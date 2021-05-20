5fcc51a51514e6ed344f262011b688f308a3c7cd
pernillevll <74861923+pernillevll@users.noreply.github.com>
2021-05-20 09:43:53 +0200

    Create pernille.js

---
+++ pernille.js
@@ -0,0 +1,21 @@
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('nav');
+
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+
+});
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(e) {
+  if (!e.target.matches('.dropbtn')) {
+  var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
